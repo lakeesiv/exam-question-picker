7b17c5381b27a3697fb3601c988da60862e73c23
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 12:26:13 +0100

    updates to start page

--- pages/start.tsx
+++ pages/start.tsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { Box, Button, Heading, Stack, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useStopwatch } from "react-timer-hook";
+import useLocalStorage from "use-local-storage";
+import ConditionalRender from "../components/ConditionalRender";
+import Stopwatch from "../components/Stopwatch";
+import DefaultSettings from "../defaults";
+import { Log, YearRange, SubjectWeightings, Question } from "../types";
+import { getLinks, getRandomQuestion } from "../utils";
+
+const pickQuestionHandler = (
+  s: SubjectWeightings,
+  y: YearRange,
+  setCurrentQuestion: React.Dispatch<React.SetStateAction<Question | null>>
+) => {
+  const q = getRandomQuestion(s, y);
+  setCurrentQuestion(q);
+};
 
 const Start = () => {
-  return <div>Start</div>;
+  const [SubjectWeightings] = useLocalStorage<SubjectWeightings>(
+    "SubjectWeightings",
+    DefaultSettings.subjectWeightings
+  );
+  const [Logs, setLogs] = useLocalStorage<Log[] | []>(
+    "Logs",
+    DefaultSettings.logs
+  );
+  const [Years] = useLocalStorage<YearRange>("Years", [1999, 2019]);
+
+  const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null);
+  const stopwatch = useStopwatch({ autoStart: false });
+  return (
+    <Box>
+      <Button
+        onClick={() =>
+          pickQuestionHandler(SubjectWeightings, Years, setCurrentQuestion)
+        }
+      >
+        New Question
+      </Button>
+      {currentQuestion && (
+        <div>
+          <Heading>
+            {currentQuestion?.subject} {currentQuestion?.year} Q
+            {currentQuestion?.question}
+          </Heading>
+
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={getLinks(currentQuestion!).paper}
+          >
+            <Button bgColor="green.700">Open Paper</Button>
+          </a>
+
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={getLinks(currentQuestion!).cribs}
+          >
+            <Button bgColor="blue.700">Open Crib</Button>
+          </a>
+          <Stopwatch stopwatch={stopwatch}></Stopwatch>
+        </div>
+      )}
+    </Box>
+  );
 };
 
 export default Start;
