6419a733a3c425806dbc6659d047f8bfdc16974f
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 10:30:56 +0100

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
-import {
-  addLog,
-  getLinks,
-  getRandomQuestion,
-  getSubjectPMF,
-} from "../utils/index";
+import React from "react";
+import { addLog, getLinks, getRandomQuestion } from "../utils/index";
 import useLocalStorage from "use-local-storage";
 import Stopwatch from "../components/Stopwatch";
 import DefaultSettings from "../defaults/index";
 import SubjectsPMFGraph from "../components/SubjectsPMFGraph";
 import { useStopwatch } from "react-timer-hook";
-import { Log, SubjectWeightings } from "../types";
-import SimpleSidebar from "../components/Sidebar";
+import { Log, SubjectWeightings, YearRange } from "../types";
 
 export default function Home() {
   const [SubjectWeightings, setSubjectWeightings] =
@@ -20,6 +14,7 @@ export default function Home() {
       DefaultSettings.subjectWeightings
     );
   const [Logs, setLogs] = useLocalStorage<Log[] | []>("Logs", []);
+  const [Years, setYears] = useLocalStorage<YearRange>("Years", [1999, 2019]);
   const stopwatch = useStopwatch({ autoStart: false });
 
   return (
@@ -33,7 +28,7 @@ export default function Home() {
       </button>
       <button
         onClick={() => {
-          const question = getRandomQuestion(SubjectWeightings, [1999, 2018]);
+          const question = getRandomQuestion(SubjectWeightings, Years);
           console.log(question, getLinks(question));
         }}
       >
