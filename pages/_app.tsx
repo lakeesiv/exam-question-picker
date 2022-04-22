5f7ef6c9c22ae1172736f1bb66055561a86f91ae
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 12:10:16 +0100

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "../components/Theme/theme";
 import React from "react";
 import SimpleSidebar from "../components/Sidebar";
+import useLocalStorage from "use-local-storage";
+import DefaultSettings from "../defaults/index";
+import { Log, SubjectWeightings, YearRange } from "../types";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [SubjectWeightings, setSubjectWeightings] =
+    useLocalStorage<SubjectWeightings>(
+      "SubjectWeightings",
+      DefaultSettings.subjectWeightings
+    );
+  const [Logs, setLogs] = useLocalStorage<Log[] | []>(
+    "Logs",
+    DefaultSettings.logs
+  );
+  const [Years, setYears] = useLocalStorage<YearRange>("Years", [1999, 2019]);
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,17 @@
 import React from "react";
 import useLocalStorage from "use-local-storage";
 import DefaultSettings from "../defaults/index";
-import { Log, SubjectWeightings, YearRange } from "../types";
-import { addLog, getRandomQuestion } from "../utils/index";
+import { Log } from "../types";
 import Activity from "../components/Activity";
+import { Heading, VStack } from "@chakra-ui/react";
 
 export default function Home() {
-  const [SubjectWeightings, setSubjectWeightings] =
-    useLocalStorage<SubjectWeightings>(
-      "SubjectWeightings",
-      DefaultSettings.subjectWeightings
-    );
-  const [Logs, setLogs] = useLocalStorage<Log[] | []>(
-    "Logs",
-    DefaultSettings.logs
-  );
-  const [Years, setYears] = useLocalStorage<YearRange>("Years", [1999, 2019]);
+  const [Logs] = useLocalStorage<Log[] | []>("Logs", DefaultSettings.logs);
 
   return (
-    <div>
-      <button
-        onClick={() =>
-          addLog(getRandomQuestion(SubjectWeightings, Years), setLogs)
-        }
-      >
-        Add Random
-      </button>
+    <VStack pt="12">
+      <Heading>Activity</Heading>
       <Activity Logs={Logs}></Activity>
-    </div>
+    </VStack>
   );
 }
