7e1ba2b644ff66cf8953bf3f5e27581594147f15
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 13:18:47 +0100

    data

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import useLocalStorage from "use-local-storage";
+import DataTable from "../components/DataTable";
+import DefaultSettings from "../defaults";
+import { Log } from "../types";
 
 const Data = () => {
-  return <div>Data</div>;
+  const [Logs, setLogs] = useLocalStorage<Log[] | []>(
+    "Logs",
+    DefaultSettings.logs
+  );
+
+  return <div>{Logs[0].subject && <DataTable logs={Logs} />}</div>;
 };
 
 export default Data;
