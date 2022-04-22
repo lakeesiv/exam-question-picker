ca3e3c865de50905eaf8f8c1c4cad6919da92b92
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 05:00:56 +0100

    More utils

--- types/index.ts
+++ types/index.ts
@@ -9,4 +9,4 @@ export interface SubjectWeightings {
 }
 
 export type Subject = "2P1" | "2P2" | "2P3" | "2P4" | "2P5" | "2P6" | "2P7";
-export type Question = 1 | 2 | 3 | 4 | 5 | 6;
+export type YearRange = [number, number];
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,32 @@
-import { SubjectWeightings } from "../types";
+import { Subject, SubjectWeightings, YearRange } from "../types";
 
 export const getSubjectPMF = (ranks: SubjectWeightings) => {
   const weightings = Object.values(ranks);
   const sum = weightings.reduce((a, b) => a + b, 0);
 
   return weightings.map((w) => w / sum);
+};
+const weightedRandomChoice = <T>(items: T[], weights: number[]) => {
+  // https://stackoverflow.com/a/55671924/15032172
+  var i;
+
+  for (i = 0; i < weights.length; i++) weights[i] += weights[i - 1] || 0;
+
+  var random = Math.random() * weights[weights.length - 1];
+
+  for (i = 0; i < weights.length; i++) if (weights[i] > random) break;
+
+  return items[i];
+};
+const randomIntFromInterval = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const getRandomSubject = (subjectWeights: SubjectWeightings) => {
+  const pmf = getSubjectPMF(subjectWeights);
+  const subjects = Object.keys(subjectWeights) as Subject[];
+  const subject = weightedRandomChoice(subjects, pmf);
+
+  return subject;
 };
 
 export const mySubjectWeightings: SubjectWeightings = {
@@ -15,4 +37,15 @@ export const mySubjectWeightings: SubjectWeightings = {
   "2P5": 5,
   "2P6": 6,
   "2P7": 5,
+};
+
+export const getRandomQuestion = (
+  subjectWeights: SubjectWeightings,
+  yearRange: YearRange
+) => {
+  const question = randomIntFromInterval(1, 6);
+  const year = randomIntFromInterval(yearRange[0], yearRange[1]);
+  const subject = getRandomSubject(subjectWeights);
+
+  return { subject, year, question };
 };
