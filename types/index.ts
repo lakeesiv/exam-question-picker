d68766bfe0b1cb36fe2cf71ec63c157925d3abe0
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-22 05:06:17 +0100

    log type

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,7 @@
+export type Subject = "2P1" | "2P2" | "2P3" | "2P4" | "2P5" | "2P6" | "2P7";
+export type YearRange = [number, number];
+export type Marks = [number, number]; // achived, total
+
 export interface SubjectWeightings {
   "2P1": number;
   "2P2": number;
@@ -8,5 +12,13 @@ export interface SubjectWeightings {
   "2P7": number;
 }
 
-export type Subject = "2P1" | "2P2" | "2P3" | "2P4" | "2P5" | "2P6" | "2P7";
-export type YearRange = [number, number];
+export interface LogObject {
+  subject: Subject;
+  year: number;
+  question: number;
+  attempted?: boolean;
+  marks?: Marks;
+  secondsTaken?: number;
+  comments?: string;
+  timeOfSubmission?: Date;
+}
