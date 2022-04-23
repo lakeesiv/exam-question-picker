f6f5e9c92acd7ed229d3fe730f8c1afebf3789c7
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-04-23 11:27:33 +0100

    extra mechanics checks

---
+++ defaults/mechanics.ts
@@ -0,0 +1,12 @@
+interface validMechanicsQuestions {
+  [key: number]: number[];
+}
+export const validMechanicsQuestions: validMechanicsQuestions = {
+  2012: [1, 3, 4, 5],
+  2013: [1, 2, 4],
+  2014: [3, 4, 5, 6],
+  2015: [1, 2, 3, 5],
+  2016: [1, 3, 6],
+  2017: [3, 4, 6],
+  2018: [2, 3, 4, 6],
+};
